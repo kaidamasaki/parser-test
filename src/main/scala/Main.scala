2c3382ab7b80edadba91b5a6eb69b3dc587d8e45
Kaida Masaki <kaida.masaki@socrata.com>
2016-06-30 13:32:59 -0700

    Implement JSON.stringify

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,7 +1,6 @@
 object Main {
   def main(args: Array[String]) {
-    // val spec = """[col9001,(col5),trim(col4)+'--'+col3,col2+";"+col1,slice(col6,8),slice(col7,0,4),{f:col9,f2:{g:col10},f3:{}},col11.replace(/m/,"h"),/india/gi]"""
-    val spec = """[col9001,(col5),trim(col4)+'--'+col3,col2+";"+col1,slice(col6,8),slice(col7,0,4),{f:col9,f2:{g:col10},f3:{}},col11.replace(/m/,"h"),/india/gi]"""
+    val spec = """[col9001,(col5),trim(col4)+'--'+col3,col2+";"+col1,slice(col6,8),slice(col7,0,4),JSON.stringify({f:col9,f2:{g:col10},f3:{}}),col11.replace(/m/,"h"),col12.replace(/out/i,"In").replace(/i/gi,"i")]"""
     val row = List("lie","char",
                    "vo"," bra ",
                    "alfa",
@@ -10,7 +9,8 @@ object Main {
                    "ignored",
                    "foxtrot",
                    "golf",
-                   "motel")
+                   "motel",
+                   "OUTdIa")
 
     val ast = transform.ast.Transform(spec)
     println(ast)
--- src/main/scala/transformer/ast/package.scala
+++ src/main/scala/transformer/ast/package.scala
@@ -115,26 +115,24 @@ package object ast {
   case class Func(name: String, args: List[Expr], self: Option[Expr]) extends Expr {
     def parseArgs(bindings: Map[String, String]) = sequence(args.map(_(bindings)))
 
-    def apply(bindings: Map[String, String]) = {
-      (name, args, self) match {
-        case ("+", _, None) => parseArgs(bindings).collect { case left::right::Nil => left + right }
-        // TODO: Add method variants of trim and slice?
-        case ("trim", _, None) => parseArgs(bindings).collect { case str::Nil => str.trim }
-        case ("slice", expr::(beg: IntLiteral)::Nil, None) =>
-          expr(bindings).map { str => str.slice(beg.value, str.length) }
-        case ("slice", expr::(beg: IntLiteral)::(end: IntLiteral)::Nil, None) =>
-          expr(bindings).map { str => str.slice(beg.value, end.value) }
-        case ("replace", RegexLiteral(pattern, global, _)::(toExpr: Expr)::Nil, Some(fromExpr)) =>
-          for {
-            from <- fromExpr(bindings);
-            to <- toExpr(bindings)
-          } yield {
-            val matcher = pattern.matcher(from)
-            if (global) matcher.replaceAll(to) else matcher.replaceFirst(to)
-          }
-        case _ =>
-          None
-      }
+    def apply(bindings: Map[String, String]) = (name, args, self) match {
+      case ("+", _, None) => parseArgs(bindings).collect { case left::right::Nil => left + right }
+      // TODO: Add method variants of trim and slice?
+      case ("trim", _, None) => parseArgs(bindings).collect { case str::Nil => str.trim }
+      case ("slice", expr::(beg: IntLiteral)::Nil, None) =>
+        expr(bindings).map { str => str.slice(beg.value, str.length) }
+      case ("slice", expr::(beg: IntLiteral)::(end: IntLiteral)::Nil, None) =>
+        expr(bindings).map { str => str.slice(beg.value, end.value) }
+      case ("replace", RegexLiteral(pattern, global, _)::(toExpr: Expr)::Nil, Some(fromExpr)) =>
+        for {
+          from <- fromExpr(bindings);
+          to <- toExpr(bindings)
+        } yield {
+          val matcher = pattern.matcher(from)
+          if (global) matcher.replaceAll(to) else matcher.replaceFirst(to)
+        }
+      case ("stringify", (obj:Object)::Nil, Some(Id("JSON"))) => obj(bindings)
+      case _ => None
     }
   }
 
