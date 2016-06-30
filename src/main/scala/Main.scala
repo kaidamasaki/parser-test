bc5b2578c7bf10bd4f8ef0744b22a442f2178c69
Kaida Masaki <kaida.masaki@socrata.com>
2016-06-29 17:34:00 -0700

    Add Object and Regex literals

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,15 +1,16 @@
 object Main {
   def main(args: Array[String]) {
-    val spec = """[(col5),trim(col4)+'--'+col3,col2+";"+col1,slice(col6,8),slice(col7,0,4),col9]"""
+    val spec = """[col9001,(col5),trim(col4)+'--'+col3,col2+";"+col1,slice(col6,8),slice(col7,0,4),{f:col9,f2:{g:col10},f3:{}},/foo/]"""
     val row = List("lie","char",
                    "vo"," bra ",
                    "alfa",
                    "a small delta",
                    "echo echo echo!",
                    "ignored",
-                   "foxtrot")
+                   "foxtrot",
+                   "golf")
 
-    val ast = transform.ImportTransformer(spec)
+    val ast = transform.ast.Transform(spec)
     println(ast)
     ast match {
       case Some(parsed) => println(parsed(row))
--- src/main/scala/transformer/ast/package.scala
+++ src/main/scala/transformer/ast/package.scala
@@ -1,5 +1,10 @@
 package transform
 
+import scala.util.matching.Regex
+
+import com.rojoma.json.v3.util.JsonUtil
+import com.rojoma.json.v3.ast.{JNumber, JObject, JString, JValue}
+
 package object ast {
   def sequence[T](list: List[Option[T]]) =
     if (list.forall(_.isDefined)) Some(list.collect { case Some(item) => item }) else None
@@ -21,7 +26,7 @@ package object ast {
   }
 
   object Transform {
-    private val regexp = """^\[(.*)\]$""".r
+    private val regex = """^\[(.*)\]$""".r
 
     def parseBody(tokens: List[String], exprs: List[Expr] = Nil): Option[List[Expr]] = {
       if (tokens == Nil) {
@@ -38,15 +43,31 @@ package object ast {
     }
 
     def apply(definition: String): Option[Transform] = definition match {
-      case regexp(inner) =>
+      case regex(inner) =>
         val tokens = inner.split("((?<=[^a-zA-Z0-9])|(?=[^a-zA-Z0-9]))").toList
         parseBody(tokens).map(Transform(_))
       case _ => None
     }
   }
 
-  trait Expr extends AstNode
+  trait Expr extends AstNode {
+    def toJson(bindings: Map[String, String]): Option[JValue] = apply(bindings).map(JString)
+  }
+
   object Expr extends Parser[Expr] {
+    def apply(tokens: List[String]): Result[Expr] = {
+      val expr = Id(tokens).
+        orElse(Literal(tokens)).
+        orElse(Wrapped(tokens)).
+        orElse(Object(tokens))
+
+      val ret = parseFunc(expr)
+      println(s"Expr: $expr")
+      println(s"parseFunc: $ret")
+
+      ret
+    }
+
     def concat(tokens: List[String], left: Expr): Result[Func] =
       Expr(tokens).map { case (right, rest) => Func("+", List(left, right)) -> rest }
 
@@ -59,14 +80,6 @@ package object ast {
       case e @ Some(_) => e
       case _ => None
     }
-
-    def apply(tokens: List[String]): Result[Expr] = {
-      val expr = Id(tokens).
-        orElse(Literal(tokens)).
-        orElse(Wrapped(tokens))
-
-      parseFunc(expr)
-    }
   }
 
   case class Id(name: String) extends Expr {
@@ -74,20 +87,28 @@ package object ast {
   }
 
   object Id extends Parser[Id] {
-    private val regexp = """^[a-zA-Z][a-zA-Z0-9]+$""".r
+    private val underlyingRegex = """^[a-zA-Z][a-zA-Z0-9]*$""".r
+    def regex(s: String): java.util.regex.Matcher = underlyingRegex.pattern.matcher(s)
 
-    def apply(tokens: List[String]) = if (regexp.pattern.matcher(tokens.head).matches) {
-      Some(Id(tokens.head) -> tokens.tail)
-    } else {
-      None
-    }
+    def apply(tokens: List[String]) =
+      if (tokens.nonEmpty && regex(tokens.head).matches && tokens.tail.head != ":") {
+        Some(Id(tokens.head) -> tokens.tail)
+      } else {
+        None
+      }
   }
 
   object Wrapped extends Parser[Expr] {
-    def apply(tokens: List[String]) = Expr(tokens.tail) match {
-      case Some((expr, rest)) if (tokens.head == "(" && rest.head == ")") =>
-        Some(expr -> rest.tail)
-      case _ => None
+    def apply(tokens: List[String]) = {
+      if (tokens.isEmpty) {
+        None
+      } else {
+        Expr(tokens.tail) match {
+          case Some((expr, rest)) if (tokens.head == "(" && rest.head == ")") =>
+            Some(expr -> rest.tail)
+          case _ => None
+        }
+      }
     }
   }
 
@@ -139,34 +160,63 @@ package object ast {
     }
   }
 
-  class Literal(body: String, delim: Option[String]) extends Expr {
+  class Literal(val body: String, val delim: Option[String]) extends Expr {
     def apply(bindings: Map[String, String]) = Some(body)
 
-    override def toString = delim.map { d => s"Literal(${d}${body}${d})" }.getOrElse(body)
+    override def toString = {
+      val inner = delim.map { d => s"${d}${body}${d}" }.getOrElse(body)
+
+      s"Literal(${inner})"
+    }
   }
 
-  case class IntLiteral(value: Int) extends Literal(value.toString, None)
+  case class IntLiteral(value: Int) extends Literal(value.toString, None) {
+    override def toJson(bindings: Map[String, String]) = Some(JNumber(value))
+  }
+
+  case class RegexLiteral(regex: Regex, flags: Set[Char])
+      extends Literal(s"/${regex}/${flags.mkString}", Some("/"))
 
   object Literal extends Parser[Literal] {
     def apply(tokens: List[String]) = {
       if (tokens.isEmpty) {
         None
       } else {
-        quote("\"", tokens).orElse(quote("'", tokens)).orElse(int(tokens))
+        quote("\"", tokens).
+          orElse(quote("'", tokens)).
+          orElse(regex(tokens)).
+          orElse(int(tokens)).
+          orElse(objectKey(tokens))
       }
     }
 
+    def unapply(literal: Literal): Option[(String, Option[String])] = Some((literal.body, literal.delim))
+
     def apply(body: String, delim: Option[String]) = new Literal(body, delim)
+
+    private def objectKey(tokens: List[String]) = {
+      if (Id.regex(tokens.head).matches && tokens.tail.head == ":") {
+        Some(Literal(tokens.head, None) -> tokens.tail)
+      } else {
+        None
+      }
+    }
 
     private def quote(delim: String, tokens: List[String]) = {
       val body = tokens.tail.takeWhile(_ != delim)
       val rest = tokens.tail.dropWhile(_ != delim)
 
-      if (!rest.isEmpty && tokens.head == delim && rest.head == delim) {
+      if (rest.nonEmpty && tokens.head == delim && rest.head == delim) {
         Some(Literal(body.mkString, Some(rest.head)) -> rest.tail)
       } else {
         None
       }
+    }
+
+    private def regex(tokens: List[String]) = quote("/", tokens) match {
+      case Some((Literal(pattern, _), head::tail)) => Some(RegexLiteral(pattern.r, head.toSet) -> tail)
+      case Some((Literal(pattern, _), Nil)) => Some(RegexLiteral(pattern.r, Set.empty) -> Nil)
+      case _ => None
     }
 
     private def int(tokens: List[String]) = {
@@ -178,13 +228,43 @@ package object ast {
     }
   }
 
-  case class Object(underlying: Map[String, Expr]) extends Expr {
-    def apply(bindings: Map[String, String]) = ???
+  case class Object(underlying: Map[Expr, Expr]) extends Expr {
+    def apply(bindings: Map[String, String]) = toJson(bindings).map(JsonUtil.renderJson(_))
+
+    override def toJson(bindings: Map[String, String]) = {
+      val evaluated: Map[Option[String], Option[JValue]] =
+        underlying.collect { case (keyExp, valExp) => keyExp(bindings) -> valExp.toJson(bindings) }
+
+      if (evaluated.forall { case (k, v) => k.isDefined && v.isDefined }) {
+        val obj = evaluated.collect { case (Some(key), Some(value)) => key -> value }
+
+        Some(JObject(obj))
+      } else {
+        None
+      }
+    }
   }
 
-  object Object {
-    def parseBody(tokens: List[String]) = ???
+  object Object extends Parser[Object] {
+    def apply(tokens: List[String]) = tokens match {
+      case "{"::"}"::tail => Some(Object(Map[Expr, Expr]()) -> tail)
+      case "{"::tail => parseBody(tokens.tail)
+      case _ => None
+    }
 
-    def apply(tokens: List[String]) = ???
+    def parseBody(tokens: List[String],
+                  lastKey: Option[Expr] = None,
+                  pairs: List[(Expr, Expr)] = Nil): Result[Object] = Expr(tokens) match {
+      case Some((key, ":"::tail)) =>
+        parseBody(tail, Some(key), pairs)
+      case Some((value, ","::tail)) => lastKey.flatMap { k =>
+        parseBody(tail, None, (k -> value)::pairs)
+      }
+      case Some((value, "}"::tail)) =>
+        val obj: Option[Map[Expr, Expr]] = lastKey.map { k => ((k -> value)::pairs).reverse.toMap }
+
+        obj.map { o => Object(o) -> tail }
+      case _ => None
+    }
   }
 }
